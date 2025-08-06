bedd780d7669b5d0052c119639b6966fdd33917b
Pool Nuñez <74687129+pooldeveloper@users.noreply.github.com>
2025-08-06 12:19:36 -0500

    actualizando año

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         < footer className="py-5 bg-dark" >
             <div className="container">
                 <p className="m-0 text-center text-white">
-                    Copyright © Uñas Sanas 2024 
+                    Copyright © Uñas Sanas 2025
                     <br />
                     +51 950936743
                 </p>
